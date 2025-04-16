0b9da3c075d8ab613cb07c908d4be792d9555baf
Shuang Yu <shuangy@shuangy-mlt.client.nvidia.com>
2025-04-16 13:36:22 +0800

    Update license description

--- rl-tutorial/r1-zero/test_aime24.py
+++ rl-tutorial/r1-zero/test_aime24.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Bytedance Ltd. and/or its affiliates
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
